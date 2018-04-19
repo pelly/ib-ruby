6b25472247a949641b21b75a569346f3809e59b5
John Pelly <jpelly@jpbfa.local>
2018-04-19 17:10:06 -0400

    Slight tweaks (Example of failure to modify an existing order obtained from RequestOpenOrders)

--- example/modify_existing_order_failure_example.rb
+++ example/modify_existing_order_failure_example.rb
@@ -6,9 +6,9 @@ class ModifyExistingOrderFailureExample
              price: 1,  # crazy price so it doesn't execute
              tif: "GTC",
              action: :buy,
-             open_close: "O"
+             open_close: "O",
+             account: account
             }
-    attrs.merge!(account:account) if account
     order = IB::Limit.order(**attrs)
 
     order_id = client.place_order(order, contract)
@@ -18,7 +18,6 @@ class ModifyExistingOrderFailureExample
     client.subscribe(:OpenOrder) { |msg| open << msg }
     client.send_message(:RequestOpenOrders)
     client.wait_for(:OpenOrderEnd, 10)
-    binding.pry
     found = open.find { |o| o.data[:order][:local_id] == order_id }
     raise "Can't find open orders matching the one just placed" if found.nil?
     modified_from_read_order = IB::Order.new(found.data[:order])
